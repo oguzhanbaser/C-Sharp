939ac56e54ae765d99a713665c777f26b7dd7293
Oğuzhan Başer <baser61061@gmail.com>
2017-05-07 00:08:59 +0300

    tcp client

---
+++ tcpClient/tcpClient/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace tcpClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bağlan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(261, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bağlantı Yok";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "LED 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MistyRose;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(10, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(229, 293);
+            this.panel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(139, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "--";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(139, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "--";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 80);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "LED 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MistyRose;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(10, 11);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(467, 70);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MistyRose;
+            this.panel3.Controls.Add(this.checkBox2);
+            this.panel3.Controls.Add(this.checkBox1);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Location = new System.Drawing.Point(245, 91);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(232, 293);
+            this.panel3.TabIndex = 5;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(47, 191);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(81, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Sürekli Oku";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(47, 61);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Sürekli Oku";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(155, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "--";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(31, 16);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 39);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "LM35";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(155, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "--";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(31, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 39);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "LDR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 398);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ tcpClient/tcpClient/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace tcpClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TcpClient myClient;
+        NetworkStream ns;
+        bool connected = false;
+
+        //gönderilecek karakterler
+        char[] led1 = {'a'};
+        char[] lm35 = { 'b' };
+        char[] led2 = { 'c' };
+        char[] ldr = { 'd' };
+        byte[] readData = new byte[256];
+        string[] stringSeparators = new string[] { "|" };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer lar 200ms' de tetikleniyor
+            timer1.Interval = 200;  
+            timer2.Interval = 200;
+        }
+
+        //bağlanma butonu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //bağlı ise bağlantıyı kes
+            if (!connected)
+            {
+                try
+                {
+                    myClient = new TcpClient("169.254.145.11", 61);         //server IP ve port
+                    label1.Text = "Bağlandı";
+                    connected = true;
+                    ns = myClient.GetStream();
+                    button1.Text = "DURDUR";
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Bağlantı Kurulamadı!\n\r" + ex.Message, "Baglanti Hatası", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    connected = false;
+                    button1.Text = "Bağlan";
+                }
+            }else
+            {
+                try
+                {
+                    myClient.Close();               //server ile bağlantıyı kes
+                    button1.Text = "Bağlan";
+                    label1.Text = "Bağlantı Yok";
+                    connected = false;
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Bağlantı Kurulamadı!\n\r" + ex.Message, "Baglanti Hatası", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    connected = false;
+                    button1.Text = "Bağlan";
+                    label1.Text = "Bağlantı Yok";
+                }
+            }
+        }
+
+        //led1 durum değiştirme ve durum görüntüleme
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(connected)
+            {
+                ns.Write(Encoding.GetEncoding("UTF-8").GetBytes(led1), 0, led1.Length);         //led1 karakterini gönderdik
+                int recv = ns.Read(readData, 0, readData.Length);                               //gelen veriyi aldık
+                String s = Encoding.ASCII.GetString(readData, 0, recv);
+                String[] ss = s.Split(stringSeparators, StringSplitOptions.None);               //parseleyip lable' e yazdırdık
+                label2.Text = ss[1];
+            }
+        }
+
+        //led2 durum değiştirme ve durum görüntüleme
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(connected)
+            {
+                ns.Write(Encoding.GetEncoding("UTF-8").GetBytes(led2), 0, led2.Length);         //led2 karakterini gönderdik
+                int recv = ns.Read(readData, 0, readData.Length);                               //gelen veriyi aldık
+                String s = Encoding.ASCII.GetString(readData, 0, recv);
+                String[] ss = s.Split(stringSeparators, StringSplitOptions.None);               //parseleyip lable' e yazdırdık
+                label3.Text = ss[1];
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(connected)
+            {
+                ns.Write(Encoding.GetEncoding("UTF-8").GetBytes(lm35), 0, lm35.Length);         //lm35 karakterini gönderdik
+                int recv = ns.Read(readData, 0, readData.Length);                               //gelen veriyi aldık
+                String s = Encoding.ASCII.GetString(readData, 0, recv);
+                String[] ss = s.Split(stringSeparators, StringSplitOptions.None);               //parseleyip lable' e yazdırdık
+                label5.Text = ss[1];
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(connected)
+            {
+                ns.Write(Encoding.GetEncoding("UTF-8").GetBytes(ldr), 0, ldr.Length);           //ldr karakterini gönderdik
+                int recv = ns.Read(readData, 0, readData.Length);                               //gelen veriyi aldık
+                String s = Encoding.ASCII.GetString(readData, 0, recv);
+                String[] ss = s.Split(stringSeparators, StringSplitOptions.None);               //parseleyip lable' e yazdırdık
+                label4.Text = ss[1];
+            }
+        }
+
+        //checkbox1 işaretlenince timer1' ı başlat
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                if(connected)
+                {
+                    timer1.Start();
+                }
+                else
+                {
+                    checkBox1.Checked = false;
+                }
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        //200ms 'de bir lm35 değerini iste
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                ns.Write(Encoding.GetEncoding("UTF-8").GetBytes(lm35), 0, led1.Length);
+                int recv = ns.Read(readData, 0, readData.Length);
+                String s = Encoding.ASCII.GetString(readData, 0, recv);
+                String[] ss = s.Split(stringSeparators, StringSplitOptions.None);
+                label5.Text = ss[1];
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        //200ms 'de bir ldr değerini iste
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                ns.Write(Encoding.GetEncoding("UTF-8").GetBytes(ldr), 0, led1.Length);
+                int recv = ns.Read(readData, 0, readData.Length);
+                String s = Encoding.ASCII.GetString(readData, 0, recv);
+                String[] ss = s.Split(stringSeparators, StringSplitOptions.None);
+                label4.Text = ss[1];
+            }
+            else
+            {
+                timer2.Stop();
+            }
+        }
+
+        //checkbox2 işaretlenince timer2' ı başlat
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                if (connected)
+                {
+                    timer2.Start();
+                }
+                else
+                {
+                    checkBox2.Checked = false;
+                }
+            }
+            else
+            {
+                timer2.Stop();
+            }
+        }
+
+    }
+}
